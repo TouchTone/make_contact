3472fc6cd21ad7eaafb8d4da5bdb73825c8775f5
TouchTone <touch_tone@mail.com>
2015-03-03 16:58:06 -0600

    Use -gui exe instead of .bat

--- run_pyinstaller.bat
+++ run_pyinstaller.bat
@@ -7,6 +7,9 @@ pyinstaller --noconfirm -F make_contact.spec
 set /p VER=<current_version.txt
 
 cd dist
+
+copy make_contact-gui\make_contact-gui.exe make_contact
+
 move make_contact make_contact_%VER%
 
 ..\7z.exe a -sfx make_contact_%VER%.exe  make_contact_%VER%
