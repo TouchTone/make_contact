1dd3c63bdf9cace88e38cd1c4c005aaf172dc7bf
TouchTone <touch_tone@mail.com>
2015-03-03 16:57:37 -0600

    Add UI for labels, recursion options, outdir

--- make_contact_gui.py
+++ make_contact_gui.py
@@ -2,6 +2,7 @@
 # GUI for make_contact
 
 import json
+import traceback
 
 from PySide.QtCore import *
 from PySide.QtGui import *
@@ -30,9 +31,16 @@ class MCWindow(QMainWindow):
         self.options = kwargs["options"]
         self.args    = kwargs["args"]
 
+        self.pdia = None
+
         # Set up routes to my methods
 
         self.ui.fileHeightT.stateChanged.connect(self.ui.fileHeightE.setDisabled)
+        self.ui.fileDirT.stateChanged.connect(self.ui.fileDirE.setDisabled)
+        self.ui.fileDirE.clicked.connect(self.pickOutDir)
+
+        for wid in [ self.ui.labelSizeE, self.ui.labelColorB ]:
+            self.ui.labelEnableC.stateChanged.connect(getattr(wid, "setEnabled"))
 
         self.ui.titleT.stateChanged.connect(self.ui.titleE.setDisabled)
         for wid in [ self.ui.titleSizeE, self.ui.titleStatsC, self.ui.titleT, self.ui.titleColorB ]:
@@ -42,6 +50,10 @@ class MCWindow(QMainWindow):
 
         self.ui.fileBackgroundB.clicked.connect(self.pickBackgroundColor)
         self.ui.titleColorB.clicked.connect(self.pickTitleColor)
+        self.ui.labelColorB.clicked.connect(self.pickLabelColor)
+
+        self.ui.dirsSubdirImagesB.stateChanged.connect(self.ui.dirsSubdirContactsB.setDisabled)
+        self.ui.dirsSubdirContactsB.stateChanged.connect(self.ui.dirsSubdirImagesB.setDisabled)
 
         self.ui.dirsAddB.clicked.connect(self.addDirs)
         self.ui.runB.clicked.connect(self.run)
@@ -55,6 +67,7 @@ class MCWindow(QMainWindow):
     def quit(self):        
         qApp.quit()
 
+
     def loadConfig(self):
         fileName = QFileDialog.getOpenFileName(self, "Load Config", "", "Config Files (*.conf)")
 
@@ -66,6 +79,7 @@ class MCWindow(QMainWindow):
         fh.close()
 
         self.options = opt
+        self.setFromOptions(opt)
 
 
     def saveConfig(self):
@@ -83,6 +97,15 @@ class MCWindow(QMainWindow):
 
         # Sheet Specs
         self.ui.fileExtE.setText(options['outputtype'])
+        if options['outdir'] == "auto":
+            self.ui.fileDirE.setText("")
+            self.ui.fileDirE.setDisabled(True)
+            self.ui.fileDirT.setChecked(True)
+        else:
+            self.ui.fileDirE.setText(options['outdir'])
+            self.ui.fileDirE.setEnabled(True)
+            self.ui.fileDirT.setChecked(False)
+
         self.ui.fileQualE.setText("%d" % options['quality'])
         self.ui.fileWidthE.setText("%d" % options['width'])  
         if options['height'] == -1:
@@ -128,15 +151,23 @@ class MCWindow(QMainWindow):
         self.ui.titleSizeE.setText("%d" % options['fontsize'])
         self.ui.titleStatsC.setCurrentIndex(options['tstats'] != False)
 
+        # Label Specs
+        self.ui.labelEnableC.setChecked(options['labels'])
+        self.ui.labelSizeE.setText("%d" % options['labelsize'])
+
         # Color Selections
-        for opt, wid in [(options['background'], self.ui.fileBackgroundB), (options['titlecolor'], self.ui.titleColorB)]:
+        for opt, wid in [(options['background'], self.ui.fileBackgroundB), (options['titlecolor'], self.ui.titleColorB), (options["labelcolor"], self.ui.labelColorB)]:
             rgb = ImageColor.getrgb(opt)
             col = QColor(rgb[0], rgb[1], rgb[2])
             wid.setStyleSheet("background-color: %s" % col.name());
 
         # Dirs list
-        for d in args:
-            self.ui.dirsL.addItem(d)
+        if not args is None:
+            for d in args:
+                self.ui.dirsL.addItem(d)
+
+        self.ui.dirsSubdirContactsB.setChecked(options["subdircontacts"])
+        self.ui.dirsSubdirImagesB.setChecked(options["recursive"])
 
 
     def setOptions(self):
@@ -160,10 +191,16 @@ class MCWindow(QMainWindow):
                 self.options['height'] = -1
             else:
                 self.options['height'] = int(self.ui.fileHeightE.text())
+            var = "Label Enable"
+            self.options['labels'] = self.ui.fileHeightT.isChecked()
+            var = "Label Size"
+            self.options['labelsize'] = int(self.ui.labelSizeE.text())
             var = "Overwrite"
             self.options['nooverwrite'] = self.ui.fileOverwriteC.currentIndex() == 0
             var = "File Type"
             self.options['outputtype'] = self.ui.fileExtE.text()
+            var = "Output Directory"
+            self.options['outdir'] = self.ui.fileDirE.text()
             var = "Quality"
             self.options['quality'] = int(self.ui.fileQualE.text())
             var = "Image Order"
@@ -182,6 +219,10 @@ class MCWindow(QMainWindow):
             self.options['tstats'] = self.ui.titleStatsC.currentIndex() == 1
             var = "Sheet Width"
             self.options['width'] = int(self.ui.fileWidthE.text())
+            var = "Subdir Contacts"
+            self.options['subdircontacts'] = self.ui.dirsSubdirContactsB.isChecked()
+            var = "Subdir Images"
+            self.options['subdirimages'] = self.ui.dirsSubdirImagesB.isChecked()
 
         except Exception, e:
             print "Caught %s setting %s, please correct" % (e, var)
@@ -195,23 +236,35 @@ class MCWindow(QMainWindow):
 
 
     def pickBackgroundColor(self):
-    
         rgb = ImageColor.getrgb(self.options['background'])       
         col = QColorDialog(QColor(rgb[0], rgb[1], rgb[2]) ).getColor()
-        
         self.options['background'] = col.name()
-
         self.ui.fileBackgroundB.setStyleSheet("background-color: %s" % col.name());
 
   
     def pickTitleColor(self):
-    
         rgb = ImageColor.getrgb(self.options['titlecolor'])        
         col = QColorDialog(QColor(rgb[0], rgb[1], rgb[2]) ).getColor()
-        
         self.options['titlecolor'] = col.name()
-
         self.ui.titleColorB.setStyleSheet("background-color: %s" % col.name());
+
+
+    def pickLabelColor(self):
+        rgb = ImageColor.getrgb(self.options['labelcolor'])
+        col = QColorDialog(QColor(rgb[0], rgb[1], rgb[2]) ).getColor()
+        self.options['labelcolor'] = col.name()
+        self.ui.labelColorB.setStyleSheet("background-color: %s" % col.name());
+
+
+    def pickOutDir(self):
+        dia = QFileDialog()
+        dia.setFileMode(QFileDialog.DirectoryOnly)
+        dia.setViewMode(QFileDialog.List)
+        dia.setOption(QFileDialog.ShowDirsOnly, True)
+
+        if dia.exec_():
+            self.options["outdir"] = dia.selectedFiles()[0]
+            self.ui.fileDirE.setText(dia.selectedFiles()[0])
 
 
     def dropDirs(self, event):
@@ -251,6 +304,16 @@ class MCWindow(QMainWindow):
         self.abort = True
 
 
+    def logpipe(self, level, msg):
+        if self.pdia is None:
+            return
+
+        if level > make_contact.LogLevels.INFO:
+            return
+
+        self.pdia.logT.insertPlainText(msg)
+
+
     def run(self):
         self.setOptions()
 
@@ -265,17 +328,19 @@ class MCWindow(QMainWindow):
 
         for i in xrange(self.ui.dirsL.count()):
             d = self.ui.dirsL.item(i)
-            print i,d.text(), self.options
+            ## print i,d.text(), self.options
 
             pd.label.setText("Processing %s..." % d.text())
             pd.dirsBar.setValue((i + 1) * 100. / float(self.ui.dirsL.count()))
 
             try:
-                make_contact.createContactSheet(self.options, d.text(), progress=self.progress)
+                make_contact.processFolder(self.options, d.text(), progress=self.progress)
             except Exception,e:
+                print traceback.format_exc()
                 break
 
         pdia.close()
+        self.pdia = None
 
 
 
@@ -285,6 +350,8 @@ def run(options, args):
     qapp = QApplication([])
 
     win = MCWindow(options = options, args = args)
+
+    make_contact.logpipe = win.logpipe
 
     QObject.connect(qapp, SIGNAL("lastWindowClosed()"), win, SLOT("quit()"))
 
